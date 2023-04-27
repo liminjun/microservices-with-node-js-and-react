33b6d137750fc5677df73bc99dd320846e36ff81
liminjun <liminjun@cmss.chinamobile.com>
2023-04-27 21:31:42 +0800

    feat:获取博客和对应的评论

--- blog/client/src/App.js
+++ blog/client/src/App.js
@@ -1,16 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import PostCreate from './PostCreate';
+import PostList from './PostList';
 
 function App() {
   return (
-    <div className="App">
+    <div className="container">
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header> */}
       <div>
       <h1>Create Post</h1>
         <PostCreate />
+        <hr/>
+        <h1>Posts</h1>
+        <PostList />
       </div>
     </div>
   );
