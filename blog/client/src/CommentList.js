13b981cca625d3494cd6ba671801c2374ec0b727
liminjun <liminjun@cmss.chinamobile.com>
2023-05-07 20:48:42 +0800

    feat:传入参数修改

--- blog/client/src/CommentList.js
+++ blog/client/src/CommentList.js
@@ -1,16 +1,16 @@
 import React, { useEffect, useState } from  'react';
 import axios from 'axios';
 
-export default ({ postId }) => {
-    const [comments, setComments] = useState([]);
-    const fetchData = async () => { 
-        const res = await axios.get(`http://localhost:4001/posts/${postId}/comments`);
-        setComments(res.data);
-    };
+export default ({ comments }) => {
+    // const [comments, setComments] = useState([]);
+    // const fetchData = async () => { 
+    //     const res = await axios.get(`http://localhost:4001/posts/${postId}/comments`);
+    //     setComments(res.data);
+    // };
     
-    useEffect(() => {
-        fetchData();
-    }, []);
+    // useEffect(() => {
+    //     fetchData();
+    // }, []);
     const renderComments = comments.map(comment => {
         return <li key={comment.id}>{comment.content}</li>;
     });
