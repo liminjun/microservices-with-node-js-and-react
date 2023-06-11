7343cffbb0312fa9ae3d46a6380e1e98cfd4a3ee
liminjun <liminjun@cmss.chinamobile.com>
2023-06-11 17:52:29 +0800

    feat:添加not found错误处理

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import "express-async-errors";
 import { json } from "body-parser";
 
 import { currentUserRouter } from "./routes/current-user";
@@ -7,6 +8,7 @@ import { signupRouter } from "./routes/signup";
 import { signoutRouter } from "./routes/signout";
 
 import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
 app.use(json());
@@ -15,6 +17,10 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signupRouter);
 app.use(signoutRouter);
+
+app.all("*", async (req, res, next) => {
+  next(new NotFoundError());
+});
 app.use(errorHandler);
 
 app.listen(3000, () => {
