a688716376892c703f76f72dd993e243b7cd1ac0
liminjun <liminjun@cmss.chinamobile.com>
2023-06-25 21:48:28 +0800

    feat:添加json web token

--- ticketing/auth/src/routes/signup.ts
+++ ticketing/auth/src/routes/signup.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response }  from "express";
 import { body, validationResult } from "express-validator";
+import jwt from "jsonwebtoken";
 import { RequestValidationError } from "../errors/request-validation-error";
 import { DatabaseConnectionError } from "../errors/database-connection-error";
 import { User } from "../models/user";
@@ -44,6 +45,12 @@ router.post('/api/users/signup',
     } else {
         const user = User.build({ email, password });
         await user.save();
+        // Generate JWT
+        const userJwt = jwt.sign({
+            id: user.id,
+            email: user.email
+        }, 'asdf');
+        // Store it on session object
         res.status(201).send(user);
     }
 });
