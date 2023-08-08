e40551b73b4021ed3979be654c8303e627451260
liminjun <v-leeli@hotmail.com>
2023-08-08 12:56:24 +0800

    feat: suppot JWT.

--- ticketing/auth/src/routes/current-user.ts
+++ ticketing/auth/src/routes/current-user.ts
@@ -1,23 +1,11 @@
 import express from "express";
 import jwt from "jsonwebtoken";
 
+import { currentUser } from "../middlewares/current-user";
 const router = express.Router();
 
 router.get('/api/users/currentuser', (req, res) => {
-    debugger
-    if (!req.session?.jwt) {
-        console.log('no jwt')
-        return res.send({ currentUser: null });
-    } else {
-        try {
-            console.log('start verify')
-            console.log(req.session.jwt);
-            const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
-            res.send({ currentUser: payload });
-        } catch (err) {
-            return res.send({ currentUser: null });
-        }
-    }
+    res.send({ currentUser: req.currentUser || null});
 });
 
 export { router as currentUserRouter };
